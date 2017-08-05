439d6e4e3a79facf24e3f569bc8ee89febc702d9
Isaac "Octa9on" Carroll <icarroll@users.noreply.github.com>
2017-08-05 09:34:59 -0700

    start of moving DCPU to Entitas

---
+++ Assets/Sources/Systems/DCPUStepSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Entitas;
+
+[SystemAvailability(InstanceKind.All)]
+public class DCPUStepSystem : IExecuteSystem {
+    private IGroup<GameEntity> dcpuGroup;
+
+    public DCPUStepSystem(Contexts contexts)
+    {
+        dcpuGroup = contexts.game.GetGroup(GameMatcher.DCPU);
+    }
+
+    public void Execute()
+    {
+        foreach (var e in dcpuGroup.GetEntities()) Process(e);
+    }
+
+    private void Process(GameEntity e)
+    {
+        var dcpu = e.dCPU;
+        if (dcpu != null)
+        {
+            for (int cycles=1666; cycles > 0; cycles-=1)
+            {
+                DCPU.step(dcpu.state);
+            }
+        }
+    }
+}
