d4ed945ee9e26c1550802bc93ffb786df402397e
Isaac "Octa9on" Carroll <icarroll@users.noreply.github.com>
2017-08-20 12:39:37 -0700

    start of DCPU test scene using Entitas

---
+++ Assets/DCPU/EntityCreatorDCPU.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityCreatorDCPU : EntityCreator {
+    public override GameEntity CreateEntity(Contexts contexts) {
+        var e = contexts.game.CreateEntity();
+        ContextsIdExtensions.AssignId(contexts, e);
+        e.AddDCPU(new dcpuState());
+        return e;
+    }
+
+    void Start () {
+		
+	}
+}
